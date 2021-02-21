ffc86abf032f32e8511585922c173d520304a538
harshrai654 <harshraitth2@gmail.com>
2021-02-21 17:40:05 +0530

    Routes updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
 import Home from "./Pages/Home";
-import SellerDashboard from "./Pages/SellerDashboard";
-import ProductsGrid from "./Pages/ProductsGrid";
-import ProductPage from "./Pages/ProductPage";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <Home>
-        <Switch>
-          <Route path="/seller" exact>
-            <SellerDashboard />
-          </Route>
-          <Route path="/" exact>
-            <ProductsGrid />
-          </Route>
-          <Route path="/product" exact component={ProductPage} />
-        </Switch>
-      </Home>
+      <Home />
     </BrowserRouter>
   );
 }
